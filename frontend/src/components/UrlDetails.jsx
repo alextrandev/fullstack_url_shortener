877f781086dac54268e695a3933da229dbf0504c
dk10119 <tranduclong092@gmail.com>
2024-04-17 12:38:02 +0300

    create a component to display shortened url infos

---
+++ frontend/src/components/UrlDetails.jsx
@@ -0,0 +1,35 @@
+import { Link, Typography } from "@mui/material";
+
+export default function UrlDetails({urlInput, shortUrl, longUrl, expireDate}) {
+  expireDate = expireDate.split("T")[0].split("-").reverse().join("/");
+  return (
+    <>
+        <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+          {urlInput !== '' && `Input: ${urlInput}`}
+        </Typography>
+
+        {longUrl !== '' && 
+          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+              Long URL:&nbsp;
+              <Link href={longUrl} color="primary">
+                  {longUrl}
+              </Link>
+          </Typography>
+        }
+
+        {shortUrl !== '' && 
+          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+              Short URL:&nbsp;
+              <Link href={shortUrl} color="primary">
+                  {shortUrl}
+              </Link>
+          </Typography>
+        }
+
+        <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+          {expireDate !== '' && `Expiration: ${expireDate}`}
+        </Typography>
+
+    </>
+  )
+}
